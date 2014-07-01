765939396c933726a1c963e8ba32ca7bf96dc5f4
Eric Morris <eric@zurb.com>
2014-07-01 15:03:57 -0700

    Add simple JS trigger

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,13 @@
-function addClass(id,new_class){
- var i,n=0;
-
- new_class=new_class.split(",");
-
- for(i=0;i<new_class.length;i++){
-	if((" "+document.getElementById(id).className+" ").indexOf(" "+new_class[i]+" ")==-1){
-				 document.getElementById(id).className+=" "+new_class[i];
-				 n++;
-	}
- }
-
- return n;
-}
+var el = document.querySelector('#slidey-panel');
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    var els = document.querySelectorAll('.small-slide-out-6, .small-expand-6'),
+        i = els.length;
+    while (i--) {
+      if(els[i].classList.contains('active')){
+        els[i].classList.remove('active');
+      } else {
+        els[i].classList.add('active');
+      }
+    }
+  });
